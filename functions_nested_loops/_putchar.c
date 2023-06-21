7a68662fe6a2cbab637e7fb95f52f8064e335007
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-06-21 12:48:56 -0700

    C - Functions, nested loops

---
+++ functions_nested_loops/_putchar.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Prints _putchar .
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+char name []= "_putchar";
+putchar(name);
+putchar('\n');
+return (0);
+}
+
