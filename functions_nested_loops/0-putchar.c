41f5c8914052ad9b09975f0df43726c8f63b86f9
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-06-21 13:30:24 -0700

    C - Functions, nested loops

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -7,8 +7,14 @@
 */
 int main(void)
 {
-char name []= "_putchar";
-putchar(name);
+putchar(95);
+putchar(112);
+putchar(117);
+putchar(116);
+putchar(99);
+putchar(104);
+putchar(97);
+putchar(114);
 putchar('\n');
 return (0);
 }
