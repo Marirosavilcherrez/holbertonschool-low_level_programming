a93d20d3fa15b01d48aca199f30e693d08cd4b1d
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-06-21 19:22:36 -0700

    Write a function that prints the alphabet, in lowercase

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,9 +1,12 @@
 #include <unistd.h>
+#ifndef MAIN_H
+#define MAIN_H
 /**
  * Copilettion of all function
  * proyect C-functions, nested loops
 **/
 
-int _putchar(char c)
+int _putchar(char c);
 void print_alphabet(void);
 
+#endif
