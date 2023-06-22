978806b321301161544b47bead3bc3f72aac21cc
Marirosavilcherrez <135650550+Marirosavilcherrez@users.noreply.github.com>
2023-06-21 22:21:09 -0500

    Update 2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* void print_alphabet_x10(void)- print the alphabet lowercase 10.
+* print_alphabet_x10- print the alphabet lowercase 10.
 *
 * Return: Always 0
 **/
@@ -8,7 +8,7 @@ void print_alphabet_x10(void)
 {
 char abcdario;
 int times;
-for (times = '0' ; times < '9' ; times++)
+for (times = '0' ; times < '10' ; times++)
 {
 for (abcdario = 'a'; abcdario <= 'z'; abcdario++)
 {
