98e73cef5b09ee9a1f0a5e7eda7200ee5e8b5550
Marirosavilcherrez <135650550+Marirosavilcherrez@users.noreply.github.com>
2023-06-22 18:56:42 -0500

    Update 4-isalpha.c

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -8,17 +8,9 @@
 */
 int _isalpha(int c)
 {
-char letter;
-int num = 0;
-for (letter = 'a'; letter <= 'z'; letter++)
-{
-if (letter == c)
-num = 1;
-}
-for (letter = 'A'; letter <= 'Z'; letter++)
-{
-if (letter == c)
-num = 1;
-}
-return (num);
+	if ((c >= 'a' && c <= 'z') ||
+	    (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
 }
