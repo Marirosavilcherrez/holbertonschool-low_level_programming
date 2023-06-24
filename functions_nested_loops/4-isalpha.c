1911de7681d0b3697b4969d2bae09fddafc50859
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-06-24 15:22:01 -0700

    Write a function that checks for alphabetic character.

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -6,23 +6,14 @@
 * Description:  Funtion to print lowercase and upper
 *
 */
-int _isalpha(int c);
+int _isalpha(int c)
 {
-char letter;
-char LETER;
-for (letter = 'a'; letter <= 'z'; letter++)
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 {
-if (letter == 'c')
 return (1);
-{
+}
 else 
+{
 return (0);
 }
-for (LETER = 'A'; LETER <= 'z'; LETER++)
-{
-if (letter == 'C')
-return (1);
-{
-else 
-return (0);
 }
