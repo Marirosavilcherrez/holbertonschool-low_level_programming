405f084ba7c2042a007e3b77f816124b416811bc
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-06-25 19:15:54 -0700

    The “Fizz-Buzz test” is an interview question designed to help filter

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+* main - prints programming job
+* Return: always 0
+* Description:  Funtion to print a program
+*
+*/
+int main(void)
+{
+int i = 1;
+for (; i < 100 ; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+printf("FizzBuzz ");
+else if (i % 3 == 0)
+printf("Fizz ");
+else if (i % 5 == 0)
+printf("Buzz ");
+else
+printf("%d ", i);
+}
+printf("Buzz\n");
+return(0);
+}
+
