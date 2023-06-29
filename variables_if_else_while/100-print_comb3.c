4d8bf1ea85e76946164dfb7a46b750f28bf500e4
Marirosavilcherrez <135650550+Marirosavilcherrez@users.noreply.github.com>
2023-06-29 17:48:15 -0500

    Update 100-print_comb3.c

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -19,7 +19,7 @@ if (num1 != num2 && num1 < num2)
 putchar(num1 + '0');
 putchar(num2 + '0');
 }
-if(!(firstdig =='8' && secdig =='9'))
+if(!(num1 =='8' && num2 =='9'))
 {
 putchar(',');
 putchar(' ');
