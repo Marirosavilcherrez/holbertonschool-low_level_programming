e6b8fc6fb93d00b43712bb860212ae0d9244877f
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-06-29 16:01:59 -0700

    Write a program that prints all possible different combinations of two digit

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -8,24 +8,26 @@
 */
 int main(void)
 {
-int num1;
-int num2;
-for (num1 = 0; num1 <= 9; num1++)
+int firstdig;
+int secdig;
+for (firstdig = '0'; firstdig <= '8'; firstdig++)
 {
-for (num2 = 0; num2 <= 9; num2++)
-{
-if (num1 != num2 && num1 < num2)
-{
-putchar(num1 + '0');
-putchar(num2 + '0');
+	for (secdig = '1'; secdig <= '9'; secdig++)
+	{
+		if (firstdig != secdig)
+		{
+		{
+			putchar(firstdig);
+			putchar(secdig);
+		}
+			if (!(firstdig == '8' && secdig == '9'))
+		{
+			putchar (',');
+			putchar (' ');
+		}
+		}
+	}
 }
-if(!(num1 =='8' && num2 =='9'))
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }
