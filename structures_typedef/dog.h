3aad85fb421e991c652d4a1acf317b93040da61a
Marirosavilcherrez <arqmvilcherrez@gmail.com>
2023-07-03 14:29:33 -0700

    Define a new type struct dog with the following elements

---
+++ structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* struct - prints
+* @dog : The name of the structure
+* Return: always 0
+* Description:  Funtion to print elements
+*
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+}type;
