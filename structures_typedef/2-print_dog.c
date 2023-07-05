aebc3b40b3be41040e21df3b447faf3ed59968bb
Marirosa <135650550+Marirosavilcherrez@users.noreply.github.com>
2023-07-05 10:03:54 -0500

    Update 2-print_dog.c

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -13,8 +13,8 @@ if (d->name == NULL)
 	printf("Name: (nil)\n");
 else
 	printf("Name: %s\n", d->name);
-printf("Age: %.6f\n", d->age);
 }
+printf("Age: %.6f\n", d->age);
 if (d->owner == NULL)
 {
 	printf("Owner: (nil)\n");
