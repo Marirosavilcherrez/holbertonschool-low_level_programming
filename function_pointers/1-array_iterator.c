31177a9d7c869140c87fb26973140738232f696a
Marirosa <135650550+Marirosavilcherrez@users.noreply.github.com>
2023-07-06 12:52:54 -0500

    Update 1-array_iterator.c

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -13,10 +13,12 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 size_t i;
 if ((array != NULL) || (action != NULL))
+{
 for (i = 0; i < size; i++)
 {
 if (array[i] >= 0)
 	action(array[i]);
+}
 }
 }
 
